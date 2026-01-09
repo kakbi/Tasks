23f12795932b79caad69b73447a225a2d6712c2c
Vadym Chebotar <dramvoda@gmail.com>
2026-01-09 23:59:27 +0200

    Add palindrome check with input normalization

---
+++ Algorithms/palindromeTwoPointers.js
@@ -0,0 +1,20 @@
+function isPalindrome(str) {
+    const cleanStr = str.toLowerCase().replace(/[^a-z0-9]/g, '');
+
+    let left = 0;
+    let right = cleanStr.length - 1;
+
+    while (left < right) {
+        if (cleanStr[left] !== cleanStr[right]) {
+            return false;
+        }
+        left++;
+        right--;
+    }
+
+    return true;
+}
+
+console.log(isPalindrome('racecar')); // true
+console.log(isPalindrome('level')); // true
+console.log(isPalindrome('hello')); // false
